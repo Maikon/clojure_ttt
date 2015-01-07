de8494e5453673b941fa509a9d6f84b77d8326e1
Makis Otman <makisotman@gmail.com>
2015-01-07 15:02:24 +0000

    Add function that makes a move on the board

--- spec/tic_tac_toe/board_spec.clj
+++ spec/tic_tac_toe/board_spec.clj
@@ -7,4 +7,9 @@
               (should= 9 (count (new-board))))
 
           (it "board size can vary"
-              (should= 16 (count (new-board 4)))))
+              (should= 16 (count (new-board 4))))
+
+          (it "mark a position"
+              (should=
+                ["x" 1 2 3 4 5 6 7 8]
+                (mark-position (new-board) 0 "x"))))
--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -3,3 +3,7 @@
 (defn new-board
   ([] (vec (range 0 9)))
   ([size] (vec (range 0 (* size size)))))
+
+(defn mark-position
+  [board position mark]
+  (assoc board position mark))
