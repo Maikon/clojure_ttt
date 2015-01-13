abd5f3500513b451a23433a909ca17c01580f16e
Makis Otman <makisotman@gmail.com>
2015-01-13 09:19:18 +0000

    Require only necessary methods in core namespace

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,13 +1,13 @@
 (ns tic_tac_toe.core
-  (:require [tic_tac_toe.board :refer :all :as board]
-            [tic_tac_toe.io :refer :all :as io])
+  (:require [tic_tac_toe.board :refer [new-board over?  mark-position current-mark]]
+            [tic_tac_toe.io :refer [print-board get-move]])
   (:gen-class :main true))
 
 (defn -main []
-  (loop [b (board/new-board)]
-    (io/print-board b)
-    (if (not (board/over? b))
-    (recur (board/mark-position
-             b
-             (dec (read-string (io/get-move)))
-             (board/current-mark b))))))
+  (loop [board (new-board)]
+    (print-board board)
+    (if (not (over? board))
+    (recur (mark-position
+             board
+             (dec (read-string (get-move)))
+             (current-mark board))))))
