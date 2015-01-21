413518008ac1dbf581dd32a9a31d56be0e8a5ba6
Makis Otman <makisotman@gmail.com>
2015-01-21 15:20:33 +0000

    WIP Add computer function for best move
    Needs cleaning and redefining

--- spec/tic_tac_toe/computer_player_spec.clj
+++ spec/tic_tac_toe/computer_player_spec.clj
@@ -1,6 +1,7 @@
 (ns tic_tac_toe.computer_player_spec
   (:require [speclj.core :refer :all]
-            [tic_tac_toe.computer_player :refer :all]))
+            [tic_tac_toe.computer_player :refer :all]
+            [tic_tac_toe.player :refer :all]))
 
 (describe "Computer Player"
   (context "finding opponent"
@@ -8,4 +9,37 @@
       (should= "o" (opponent-of "x")))
 
     (it "for 'o'"
-      (should= "x" (opponent-of "o")))))
+      (should= "x" (opponent-of "o"))))
+
+  (it "goes for the win"
+    (should=
+      2
+      (get-move {:type :computer :board ["x" "x" ""
+                                         "o" "o" ""
+                                         "" "" ""]})))
+  (it "blocks opponent's win"
+    (should=
+      4
+      (get-move {:type :computer :board ["x" "" ""
+                                         "o" "" "o"
+                                         "" "x" ""]})))
+  (it "blocks diagonal fork"
+    (should=
+      7
+      (get-move {:type :computer :board ["x" "" ""
+                                         "" "o" ""
+                                         "" "" "x"]})))
+
+  (it "blocks alternative diagonal fork"
+    (should=
+      6
+      (get-move {:type :computer :board ["x" "" ""
+                                         "" "x" ""
+                                         "" ""  "o"]})))
+
+  (it "blocks edges fork"
+    (should=
+      8
+      (get-move {:type :computer :board ["" "" ""
+                                         "" "o" "x"
+                                         "" "x" ""]}))))
--- src/tic_tac_toe/computer_player.clj
+++ src/tic_tac_toe/computer_player.clj
@@ -1,6 +1,42 @@
-(ns tic_tac_toe.computer_player)
+(ns tic_tac_toe.computer_player
+  (:require [tic_tac_toe.player :as player]
+            [tic_tac_toe.board :refer :all]))
 
 (defn opponent-of [mark]
   (cond
     (= "x" mark) "o"
     :else "x"))
+
+(defn score-board [board]
+  (let [score (/ 10.0 (- (count board)
+                        (count (available-moves board))))]
+  (cond
+    (winner? board (current-mark board)) score
+    (and (over? board) (not (winner? board))) 0
+    :else (unchecked-negate score))))
+
+(defn next-board-with-move [board move]
+  {:board (mark-position board move (current-mark board))
+   :move move})
+
+(defn get-nodes [board]
+  (map
+    (partial next-board-with-move board)
+    (available-moves board)))
+
+(defn score [board]
+  (let [moves (available-moves board)]
+    (if (over? board)
+      (score-board board)
+      (apply max (map #(- (score (mark-position board % (current-mark board)) )) moves)))))
+
+(defn score-and-move [mark {:keys [board move]}]
+  {:score (- (score board))
+    :move  move})
+
+(defmethod player/get-move :computer [player]
+  (:move (apply max-key :score
+                (map
+    (partial score-and-move
+             (current-mark (:board player)))
+    (get-nodes (:board player))))))
